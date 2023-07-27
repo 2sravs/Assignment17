8c2664eb42e05b1197e7204d2c105096f865ae42
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-27 15:49:04 +0530

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(8);
+            circle.Print();
+
+            Rectangle rectangle = new Rectangle(9, 6);
+            rectangle.Print();
+
+            Triangle triangle = new Triangle(1, 3, 8);
+            triangle.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17
+{
+    public class Shape
+    {
+       
+        public virtual string ShapeType { get; set; }
+        public virtual double Area { get; }
+        public virtual double Perimeter { get;  }
+        public void Print()
+        {
+            Console.WriteLine("ShapeType:"+ShapeType);
+            Console.WriteLine("Area:"+Area);
+            Console.WriteLine("Perimeter:"+Perimeter);
+            Console.WriteLine("\n");
+        }
+
+       
+       
+    }
+}
